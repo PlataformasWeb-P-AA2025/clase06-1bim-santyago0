dbbf1d8db6b51389fe656fdf7705cd19362d7ca5
santyago0 <santyago0515@gmail.com>
2025-05-14 09:05:28 -0500

    Agregar registros a la base y presentarlos con streamlit

--- ejemplo1/agregar_data2.py
+++ ejemplo1/agregar_data2.py
@@ -8,10 +8,26 @@ import csv
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Creamos un objeto de tipo Saludo2
-
-miSaludo = Saludo2()
+data = []
 
 # Leer el csv
 with open('data/saludos_mundo.csv', mode='r', encoding='utf-8') as data_csv:
-	data = csv.DictReader(data_csv, delimiter='|')
+	diccionario = csv.DictReader(data_csv, delimiter='|')
+
+	# Agregar la data a la lista
+	for d in diccionario:
+		data.append(d)
+
+# Bucle para agregar los registros a la base de datos
+for d in data:
+	# Creamos un objeto de tipo Saludo2
+	miSaludo = Saludo2()
+	miSaludo.mensaje = d["saludo"]
+	miSaludo.tipo = d["tipo"]
+	miSaludo.origen = d["origen"]
+
+	# Agregar el objeto Saludo2
+	session.add(miSaludo)
+
+# Confirmar las transacciones
+session.commit()
--- ejemplo1/app_streamlit2.py
+++ ejemplo1/app_streamlit2.py
@@ -1,4 +1,28 @@
 import streamlit as st
 from sqlalchemy.orm import sessionmaker
-from crear_base import Saludo
+from crear_base import Saludo2
 from configuracion import engine
+
+# Crear sesión
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Consultar saludo_dos
+saludos = session.query(Saludo2).all()
+
+# Mostrar con Streamlit
+st.title("Presentación de todos los Saludo_dos")
+
+for saludo  in saludos:
+    st.write(saludo)
+    st.markdown("---")
+
+st.markdown("---")
+st.title("Presentación de todos los Saludo_dos en Tabla")
+lista = []
+
+for s in saludos:
+    diccionario = {"id": s.id, "mensaje": s.mensaje, "tipo": s.tipo, "origen": s.origen}
+    lista.append(diccionario)
+
+st.dataframe(lista)
